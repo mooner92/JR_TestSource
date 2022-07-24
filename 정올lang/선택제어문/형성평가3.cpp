256015a655edc1b27fb41d22949d825f8e59ed75
Mooner92 <chlaudgjs0@naver.com>
2022-07-24 10:14:21 +0900

    add

---
+++ 정올lang/선택제어문/형성평가3.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int year;
+    scanf("%d", &year);
+    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+        printf("leap year");
+    else
+        printf("common year");
+}
