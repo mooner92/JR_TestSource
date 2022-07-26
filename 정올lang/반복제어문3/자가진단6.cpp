eb5009c16dfb8328a412689442a3c699eacad02c
Mooner92 <chlaudgjs0@naver.com>
2022-07-26 14:15:04 +0900

    add

--- 정올lang/반복제어문3/자가진단6.cpp
+++ 정올lang/반복제어문3/자가진단6.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int n;
+    int n, p = 65; // ASCII 65 = 'A'
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i; j++)
-            printf(" ");
         for (int k = 0; k < n - i; k++)
-            printf("*");
+        {
+            printf("%c", p);
+            p++;
+        }
         printf("\n");
     }
 }
