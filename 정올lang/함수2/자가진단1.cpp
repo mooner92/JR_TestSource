297a144b55e75afd33de43e287ef275d96a3ed9d
Mooner92 <chlaudgjs0989@gmail.com>
2022-08-05 19:13:21 +0900

    add

---
+++ 정올lang/함수2/자가진단1.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define SWAP(x, y, temp) ((temp) = (x), (x) = (y), (y) = (temp))
+#define MAX_SIZE 5
+
+// 선택 정렬
+void selection_sort(int list[], int n)
+{
+    int i, j, least, temp;
+
+    // 마지막 숫자는 자동으로 정렬되기 때문에 (숫자 개수-1) 만큼 반복한다.
+    for (i = 0; i < n - 1; i++)
+    {
+        least = i;
+
+        // 최솟값을 탐색한다.
+        for (j = i + 1; j < n; j++)
+        {
+            if (list[j] > list[least])
+                least = j;
+        }
+
+        // 최솟값이 자기 자신이면 자료 이동을 하지 않는다.
+        if (i != least)
+        {
+            SWAP(list[i], list[least], temp);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    int ds[11] = {0};
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ds[i]);
+    }
+    selection_sort(ds, n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", ds[i]);
+    }
+}
