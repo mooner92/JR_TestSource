d28a4b2066b6ee030155fa1fdecb53f653787705
Mooner92 <chlaudgjs0989@gmail.com>
2022-08-14 09:33:27 +0900

    add

--- 정올beginner/문자열/변장.cpp
+++ 정올beginner/문자열/변장.cpp
@@ -13,6 +13,25 @@ int main()
         for (int j = 0; j < n; j++)
         {
             scanf("%*s %s", str[j]); //%*s는 문자열을 저장하지 않고 무시하라는 뜻 -> %*d %*lf등등
+            //파이썬의 find개념을 사용한 풀이
+            int k;
+            for (k = 0; k <= j; k++)
+            {
+                if (strcmp(str[k], str[j]) == 0)
+                    break; //처음으로 같은값이 나온 index 지정
+            }
+            if (k != j)
+            {
+                cnts[k]++;
+            }
+            else
+                cnts[j]++;
         }
+        int ans = 1;
+        for (int j = 0; j < 30; j++)
+        {
+            ans *= (cnts[j] + 1);
+        }
+        printf("%d\n", ans - 1);
     }
 }
