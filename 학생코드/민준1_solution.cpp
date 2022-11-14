e9362061ac54c85e4022ebc0b8b0689b379e8bc5
Mooner92 <chlaudgjs0989@gmail.com>
2022-11-14 20:58:51 +0900

    add

--- 학생코드/민준1_solution.cpp
+++ 학생코드/민준1_solution.cpp
@@ -1,25 +1,33 @@
 #include <stdio.h>
-int main(){
-    int ds[101] = {0,};
-    int n,cnt=0,k;
+int main()
+{
+    int ds[101] = {
+        0,
+    };
+    int n, cnt = 0, k;
     char c;
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf(" %c",&c);
-        if(c=='i'){
-            scanf("%d",&k);
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf(" %c", &c);
+        if (c == 'i')
+        {
+            scanf("%d", &k);
             ds[++cnt] = k;
         }
-        else if(c=='o'){
-            if(cnt == 0 )
-            printf("empty\n");
-            else{
-            printf("%d\n",ds[cnt--]);
-            //cnt--;
+        else if (c == 'o')
+        {
+            if (cnt == 0)
+                printf("empty\n");
+            else
+            {
+                printf("%d\n", ds[cnt--]);
+                // cnt--;
             }
         }
-        else{
-            printf("%d\n",cnt);
+        else
+        {
+            printf("%d\n", cnt);
         }
     }
-}//////
+} ///////////
